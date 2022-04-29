a53f67a769eb8d53d303a55057337d7a90b3328b
ivoxo <oaivox@gmail.com>
2022-04-29 22:58:45 +0000

    update commands

--- discordBot/deploy-commands.js
+++ discordBot/deploy-commands.js
@@ -1,38 +1,40 @@
+const deleting = process.argv[2];
+const fs = require('node:fs');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { discordBot } = require('../config.json');
 const { clientId, guildId, token } = discordBot;
-
-const commands = [
-	new SlashCommandBuilder().setName('ping')
-    .setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('server')
-    .setDescription('Replies with server info!'),
-	new SlashCommandBuilder().setName('user')
-    .setDescription('Replies with user info!'),
-].map(command => command.toJSON());
-
 const rest = new REST({ version: '9' }).setToken(token);
-const deleting = !true;
 
 if (deleting) {
   rest.get(Routes.applicationGuildCommands(clientId, guildId))
   .then(data => {
+    console.log(`deleting commands:`);
     const promises = [];
-      for (const command of data) {
-        console.log(command);
-        const deleteUrl = `${
-          Routes.applicationGuildCommands(clientId, guildId)
-        }/${command.id}`;
-        promises.push(rest.delete(deleteUrl));
-      }
-      return Promise.all(promises);
+    for (const command of data) {
+      console.log(command.name + `,`);
+      const deleteUrl = `${
+        Routes.applicationGuildCommands(clientId, guildId)
+      }/${command.id}`;
+      promises.push(rest.delete(deleteUrl));
+    }
+    return Promise.all(promises);
   });
 }
 
 if (deleting) return;
-console.log(`registering commands...`, commands);
+
+const commands = [];
+const commandFiles = fs.readdirSync('./discordBot/commands')
+  .filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+console.log(`registering commands...`, commands.map(function(o) {
+    return o.name;
+}).join(", "));
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
   .then(() => console.log('Successfully registered application commands.'))
   .catch(console.error);
--- discordBot/index.js
+++ discordBot/index.js
@@ -1,31 +1,34 @@
 console.log(`...initializing discord bot...`);
 
-const { Client, Intents } = require('discord.js');
+const fs = require('node:fs');
+const { Client, Intents, Collection } = require('discord.js');
 const { discordBot } = require('../config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./discordBot/commands')
+  .filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
 
 client.once('ready', () => {
 	console.log('Discord Bot Ready!');
 });
 
 client.on('interactionCreate', async interaction => {
+  console.log(`interactionCreate - command:`, interaction.commandName);
 	if (!interaction.isCommand()) return;
-
-	const { commandName } = interaction;
-
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'server') {
-    await interaction.reply(
-      `Server name: ${
-        interaction.guild.name
-      }\nTotal members: ${interaction.guild.memberCount}`
-    );
-	} else if (commandName === 'user') {
-    await interaction.reply(
-      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
-    );
+  const command = client.commands.get(interaction.commandName);
+	if (!command) return;
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({
+      content: 'There was an error while executing this command!', ephemeral: true
+    });
 	}
 });
 
