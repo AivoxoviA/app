e64558e8fe17835631a3b846906e16430606e42d
ivoxo <oaivox@gmail.com>
2022-04-29 21:38:15 +0000

    update commands

--- discordBot/deploy-commands.js
+++ discordBot/deploy-commands.js
@@ -3,7 +3,6 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { discordBot } = require('../config.json');
 const { clientId, guildId, token } = discordBot;
-const { Client } = require("discord-slash-commands-client");
 
 const commands = [
 	new SlashCommandBuilder().setName('ping')
@@ -15,20 +14,25 @@ const commands = [
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(token);
+const deleting = !true;
 
-rest.get(Routes.applicationGuildCommands(clientId, guildId))
-.then(data => {
+if (deleting) {
+  rest.get(Routes.applicationGuildCommands(clientId, guildId))
+  .then(data => {
     const promises = [];
-    for (const command of data) {
+      for (const command of data) {
+        console.log(command);
         const deleteUrl = `${
           Routes.applicationGuildCommands(clientId, guildId)
         }/${command.id}`;
         promises.push(rest.delete(deleteUrl));
-    }
+      }
+      return Promise.all(promises);
+  });
+}
 
-    rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-      .then(() => console.log('Successfully registered application commands.'))
-      .catch(console.error);
-
-    return Promise.all(promises);
-});
+if (deleting) return;
+console.log(`registering commands...`, commands);
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log('Successfully registered application commands.'))
+  .catch(console.error);
--- discordBot/index.js
+++ discordBot/index.js
@@ -9,4 +9,24 @@ client.once('ready', () => {
 	console.log('Discord Bot Ready!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+    await interaction.reply(
+      `Server name: ${
+        interaction.guild.name
+      }\nTotal members: ${interaction.guild.memberCount}`
+    );
+	} else if (commandName === 'user') {
+    await interaction.reply(
+      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
+    );
+	}
+});
+
 client.login(discordBot.token);
