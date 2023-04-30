4963c1ac1d4c4bda8d2b1679193ad966a56755a6
yuulye <yuulye@gmail.com>
2023-04-30 14:24:46 +0700

    init mlbb squad

--- routes/mlbb/squad.js
+++ routes/mlbb/squad.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AivoxoviA' });
+  res.render('mlbb/squad', { title: 'AivoxoviA - MLBB Squad' });
 });
 
 module.exports = router;
