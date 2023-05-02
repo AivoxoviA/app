f79015dc39ec12bb473b6c79165a4442bd7977e6
yuulye <yuulye@gmail.com>
2023-05-02 14:26:42 +0700

    load config from config vars

--- discord/bot/index.js
+++ discord/bot/index.js
@@ -1,7 +1,24 @@
 console.log('discord bot loading...');
 
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('../../config.json').discord.bot;
+let token;
+console.log(token);
+
+const fs = require('fs');
+const configFile = 'config.json';
+
+try {
+	if (fs.existsSync(`./${configFile}`)) {
+		token = require(`../../${configFile}`).discord.bot.token;
+	} else if (process.env.config) {
+		console.log('using config env vars');
+		token = JSON.parse(process.env.config).discord.bot.token; 
+	} else {
+		throw new Error('config couldn\'t be loaded!');
+	}
+} catch (e) {
+	console.error(e);
+}
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
