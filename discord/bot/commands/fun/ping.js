b42f9458d660b4d361504f1795fe2d7c9f8d509e
yuulye <yuulye@gmail.com>
2023-05-04 03:26:12 +0700

    adding commands to discord bot

---
+++ discord/bot/commands/fun/ping.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		return interaction.reply('Pong! <a:checkmark1:751806451474890752>');
+	},
+};
