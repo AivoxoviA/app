f1392c1548b2a77775981cadb7e85a623cad2705
yuulye <yuulye@gmail.com>
2023-05-04 07:38:56 +0700

    show some mlbb heroes

--- routes/mlbb.js
+++ routes/mlbb.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mlbb/index', { title: 'AivoxoviA - MLBB' });
+  let heroes = require(
+    '../data/mlbb/heroes'
+  ).filter(hero => hero.flag);
+  console.log(heroes);
+  res.render('mlbb/index', {
+    title: 'AivoxoviA - MLBB'
+    , heroes: heroes
+  });
 });
 
 module.exports = router;
