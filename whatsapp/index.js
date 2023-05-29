7c7f136b43193df0ab2a3dd5aeae778062b14286
yuulye <yuulye@gmail.com>
2023-05-29 17:35:35 +0700

    Implement whatsapp-web.js https://wwebjs.dev/

---
+++ whatsapp/index.js
@@ -0,0 +1,21 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', (qr) => {
+  console.log('QR RECEIVED', qr);
+  qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+  console.log('Client is ready!');
+});
+
+client.on('message', message => {
+  if (message.body === '!ping') {
+    client.sendMessage(message.from, 'pong');
+  }
+});
+
+client.initialize();
